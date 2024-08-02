c6f3d323417cc07a664503eb926a726cf8d567ea
Ilia Rudiak <ilyarudyak@yahoo.com>
2024-08-02 22:51:50 +0300

    Wrote function to prompt for a password

--- 04-github-copilot/03-functions/leo_password.py
+++ 04-github-copilot/03-functions/leo_password.py
@@ -22,17 +22,33 @@ def is_strong_password_v2(password):
     # print(string.punctuation)
     return has_uppercase and has_number and has_special
 
+def get_strong_password():
+    """
+    Keep asking the user for a password until it is a strong
+    password, and return that strong password. Use the function
+    is_strong_password_v2() to check if the password is strong.
+    """
+    while True:
+        password = input('Enter a strong password: ')
+        if is_strong_password_v2(password):
+            # Print success message and return the password
+            print('Password is strong!')
+            return password
+        print('That password is not strong enough. Try again.')
+
 
 if __name__ == '__main__':
 
-    # Add some simple tests for the function is_strong_password()
-    assert is_strong_password('password') == False
-    assert is_strong_password('qwerty') == False
-    assert is_strong_password('password1') == True
+    # # Add some simple tests for the function is_strong_password()
+    # assert is_strong_password('password') == False
+    # assert is_strong_password('qwerty') == False
+    # assert is_strong_password('password1') == True
 
-    # Add some simple tests for the function is_strong_password_v2()
-    assert is_strong_password_v2('password') == False
-    assert is_strong_password_v2('qwerty') == False
-    assert is_strong_password_v2('password1') == False
-    assert is_strong_password_v2('Password1!') == True
-    assert is_strong_password_v2('N3w Y0rk J375') == False
+    # # Add some simple tests for the function is_strong_password_v2()
+    # assert is_strong_password_v2('password') == False
+    # assert is_strong_password_v2('qwerty') == False
+    # assert is_strong_password_v2('password1') == False
+    # assert is_strong_password_v2('Password1!') == True
+    # assert is_strong_password_v2('N3w Y0rk J375') == False
+
+    get_strong_password()
