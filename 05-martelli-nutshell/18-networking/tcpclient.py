edbe6f1a5ec3ae46975cbf480a1bd2492e1ec966
Ilia Rudiak <ilyarudyak@yahoo.com>
2024-08-08 18:28:08 +0300

    Done with the TCP client-server

---
+++ 05-martelli-nutshell/18-networking/tcpclient.py
@@ -0,0 +1,24 @@
+import socket
+
+IP_ADDR = 'localhost'
+IP_PORT = 8881
+MESSAGE = """\
+A few lines of text
+including non-ASCII characters: €£
+to test the operation
+of both server
+and client."""
+
+with socket.socket(socket.AF_INET,     # IPv4
+                   socket.SOCK_STREAM  # TCP
+                   ) as sock:
+    sock.connect((IP_ADDR, IP_PORT))
+    print(f'Connected to server {IP_ADDR}:{IP_PORT}')
+    for line in MESSAGE.splitlines():
+        data = line.encode('utf-8')
+        sock.sendall(data)
+        print(f'SENT {data!r} ({len(data)})')
+        response, address = sock.recvfrom(1024)  # buffer size: 1024
+        print(f'RCVD {response!r} ({len(response)}) from {address}')
+
+print('Disconnected from server')
